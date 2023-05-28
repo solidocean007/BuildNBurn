14a5e403ff60a10cf04570a0a4cbaf0789728ac9
solidocean007 <clintonwilliams007@gmail.com>
2023-05-28 08:14:50 -0400

    Build Components

--- todo-burner/src/App.jsx
+++ todo-burner/src/App.jsx
@@ -4,31 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [taskCount, setTaskCount] = useState(0)
+  const [task, setTask] = useState('');
+  cosnt [taskList, setTaskList] = useState([]);
+
+  const handleChange = () {
+    
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='todo-panel'>
+      <div className="title">Task Manager</div>
+      <div className="task-input">
+        <input type="text" value={task} onChange={handleChange} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      .
+    </div>
   )
 }
 
---
+++ todo-burner/src/TaskList.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+const TaskList = (taskList) => {
+
+  return (
+    {taskList}
+  )
+}
+
+export default TaskList;
